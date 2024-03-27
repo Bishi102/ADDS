e73abd4f11e4813485cad74b2ed30c234a24818c
Hung Huang <hung_h102@icloud.com>
2024-03-27 12:26:38 +1030

    hm

--- Finder.cpp
+++ Finder.cpp
@@ -5,7 +5,7 @@ using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
   vector<int> result;
   for (size_t i = 1; i <= s2.size(); i++) {
-    size_t found = s1.find(s2.substr(0, i-1));
+    size_t found = s1.find(s2, i-1);
     if (found != string::npos) {
       result.push_back(found);
     } else {
