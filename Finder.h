a0ad2d1a9167b46959a36ebd8e4364a3a57d8749
Hung Huang <hung_h102@icloud.com>
2024-03-27 11:49:52 +1030

    hm

---
+++ Finder.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+
+class Finder {
+    
+    public:
+        std::vector<int> findSubstrings(std::string s1, std::string s2);
+};
+
