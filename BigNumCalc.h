028aa902f2aacab5125b91adce34a66d1b9c2186
Hung Huang <hung_h102@icloud.com>
2024-04-29 20:12:48 +0930

    hm

---
+++ BigNumCalc.h
@@ -0,0 +1,12 @@
+#include <list>
+
+#pragma once
+
+class bigNumCalc {
+    public:
+    std::list<int> buildBigNum(std::string numString);
+    std::list<int> add(std::list<int> num1, std::list<int> num2);
+    std::list<int> sub(std::list<int> num1, std::list<int> num2);
+    std::list<int> mul(std::list<int> num1, std::list<int> num2);
+
+};
