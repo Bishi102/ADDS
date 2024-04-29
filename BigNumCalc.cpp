81e90d23196560dd0e016daa034e1cb5e67de721
Hung Huang <hung_h102@icloud.com>
2024-04-29 20:51:37 +0930

    hm

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -1,2 +1,21 @@
 #include "BigNumCalc.h"
 
+BigNumCalc::BigNumCalc() {}
+
+BigNumCalc::~BigNumCalc() {}
+
+std::list<int> BigNumCalc::buildBigNum(std::string numString) {
+  return std::list<int>();
+}
+
+std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {
+  return std::list<int>();
+}
+
+std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
+  return std::list<int>();
+}
+
+std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {
+  return std::list<int>();
+}
